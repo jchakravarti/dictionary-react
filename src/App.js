99b85ad8864c4b6766051226bc9cec6eb132e397
jchakravarti <79936713+jchakravarti@users.noreply.github.com>
2021-05-20 21:44:19 +0100

    Added dictionary component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
       Dictionary App
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      </div>
     </div>
   );
 }
