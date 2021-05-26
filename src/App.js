eff7b2d23d805551c418f75703b0c5a8e96564b1
jchakravarti <79936713+jchakravarti@users.noreply.github.com>
2021-05-26 22:39:59 +0100

    Implemented styling of the app

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import image from "./image.png";
 import './App.css';
 import Dictionary from "./Dictionary";
 
@@ -6,12 +7,16 @@ export default function App() {
     <div className="App">
       <div className="container">
       <header className="App-header">
-      Dictionary App
+      <img src={image} className="image img-fluid" alt="giraffe" />
       </header>
       <main>
-        <Dictionary />
+        <Dictionary defaultKeyword="crab"/>
       </main>
+      <footer>
+        This project was coded by <a href="https://www.linkedin.com/in/jayani-chakravarti-bb4b04106/" target="_blank" rel="noreferrer">Jayani Chakravarti</a> and is <a href="https://github.com/jchakravarti/dictionary-react" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
+      </footer>
       </div>
+      
     </div>
   );
 }
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,34 +4,52 @@ import "./Dictionary.css";
 import axios from "axios";
 
 
-export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+    let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
+    let [loaded, setLoaded] = useState(false);
 
     function handleResponse(response) {
         setResults(response.data[0]);
     }
 
-    function search(event) {
-        event.preventDefault();
-        
-        let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+
+function search() {
+ let apiUrl =`https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
         axios.get(apiUrl).then(handleResponse);
+}
+
+function load() {
+    setLoaded(true);
+    search();
+}
+    function handleSubmit(event) {
+        event.preventDefault();
+        search();
     }
 
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
     }
 
-    return (
+    if (loaded) {
+return (
 <div className="Dictionary">
-<form onSubmit={search}>
-    <input type="search" placeholder="What word do you want to look up?" onChange={handleKeywordChange} />
+    <section>
+        <h1> What word do you want to look up?</h1>
+<form onSubmit={handleSubmit}>
+    <input type="search" onChange={handleKeywordChange} />
 </form>
-
+<div className= "suggestions">Get definitions, synonyms and more</div>
+</section>
 <Results results={results}/>
 
 </div>
 
     )
+    } else {
+        load();
+        return "Loading";
+    }
+    
 }
